002ace13a7de21457ac97f29014f6bdf3f932026
ellos0 <mcgibbonellott@gmail.com>
2025-10-30 21:25:50 -0700

    structs but better

--- rust/structs.rs
+++ rust/structs.rs
@@ -10,8 +10,8 @@ struct Guy {
 struct Unit;
 
 struct Point {
-   x: i32,
-   y: i32,
+   x: f32,
+   y: f32,
 }
 
 struct Rectangle {
@@ -20,5 +20,19 @@ struct Rectangle {
 }
 
 fn main() {
-   println!("yay");
+  let name = String::from("Peter");
+  let age = 27;
+  let peter = Guy {name,age};
+  
+  println!("{:?}", peter);
+  
+  let point: Point = Point {x: 5.2, y: 0.4};
+  let another_point: Point = Point {x: 10.3, y: 0.2};
+
+  println!("point: ({},{})",point.x,point.y);
+  println!("point 2: ({},{})", another_point.x, another_point.y);
+  
+  //sorry not gonna do more im lazy
+
+  println!("yay");
 }
