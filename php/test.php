d1a17bdcb1698c9ea34c52e8003958f5a6c749b2
ellos0 <mcgibbonellott@gmail.com>
2026-02-01 16:07:55 -0800

    added a more formal html layout

--- php/test.php
+++ php/test.php
@@ -1,2 +1,17 @@
-<p>non php text</p>
-<?php echo 'php text'; ?>
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+    <title> PHP test programme </title>
+</head
+  
+  <body>
+    <h1> This is a test of PHP! </h1>
+    <p>non php text</p>
+    <?php
+     $toSay = "php generated text";
+     echo "<p> $toSay </p>\n";
+?>
+</body>
+</html>
